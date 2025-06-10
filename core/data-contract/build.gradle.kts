b9fcdcba8ef8f46c08e0d64f2c59ff910dbe50d3
puria kazemieh <pkazemieh@tosantechno.com>
2025-06-10 11:30:05 +0330

    [FEATURE] Add data-contract module

---
+++ core/data-contract/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.coroutines.core)
+
+}
