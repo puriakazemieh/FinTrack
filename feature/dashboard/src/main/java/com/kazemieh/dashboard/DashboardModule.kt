323c041f076a09747f449219a7cac4f6fa78a6a7
puria kazemieh <puriakazemieh@gmail.com>
2025-09-23 19:24:20 +0330

    [FEATURE] Dashboard module deleted

--- feature/dashboard/src/main/java/com/kazemieh/dashboard/DashboardModule.kt
+++
@@ -1,8 +0,0 @@
-package com.kazemieh.dashboard
-
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-
-val dashboardModule = module {
-    viewModel { DashboardViewModel(transactionUseCases = get()) }
-}
