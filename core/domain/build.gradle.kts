1078d99e0867b9e2437ee9ddd0bb74e2fd283c4c
puria kazemieh <puriakazemieh@gmail.com>
2026-02-22 20:19:27 +0330

    [REFACTOR] Change core:domain from Android library to kotlin library

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,12 +1,10 @@
 plugins {
-    id("convention.android.library")
+    id("convention.kotlin.library")
     id("convention.kotlin.serialization")
     id("convention.koin")
 }
 
-android {
-    namespace = "com.kazemieh.domain"
-}
 dependencies {
     implementation(project(":core:common"))
+    implementation(libs.kotlinx.coroutines.core)
 }
