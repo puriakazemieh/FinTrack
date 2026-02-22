c6055387297ef1efa14448447ac1cdc258c23e9c
puria kazemieh <puriakazemieh@gmail.com>
2026-02-22 20:34:39 +0330

    [REFACTOR] Change core:data from Android library to kotlin library

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,15 +1,12 @@
 plugins {
-    id("convention.android.library")
+    id("convention.kotlin.library")
     id("convention.kotlin.serialization")
     id("convention.koin")
-}
-
-android {
-    namespace = "com.kazemieh.data"
 }
 
 dependencies {
     implementation(project(":core:domain"))
     implementation(project(":core:common"))
     implementation(project(":core:data-contract"))
+    implementation(libs.kotlinx.coroutines.core)
 }
