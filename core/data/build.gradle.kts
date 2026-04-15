7d11f2f61ea5d41c57526bd83e9c3717befae7b5
puria kazemieh <puriakazemieh@gmail.com>
2026-04-15 17:16:48 +0330

    [REFACTOR] Change core:data from Android library to kmp library

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,12 +1,76 @@
 plugins {
-    id("convention.kotlin.library")
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.android.lint)
     id("convention.kotlin.serialization")
     id("convention.koin")
 }
 
-dependencies {
-    implementation(project(":core:domain"))
-    implementation(project(":core:common"))
-    implementation(project(":core:data-contract"))
-    implementation(libs.kotlinx.coroutines.core)
+kotlin {
+    androidLibrary {
+        namespace = "com.kazemieh.data"
+        compileSdk {
+            version = release(36)
+        }
+        minSdk = 24
+    }
+
+    val xcfName = "core:commonKit"
+
+    iosX64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosSimulatorArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    js {
+        browser()
+    }
+
+    jvm()
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(project(":core:common"))
+                implementation(project(":core:domain"))
+                implementation(project(":core:data-contract"))
+            }
+        }
+
+
+        androidMain {
+            dependencies {
+            }
+        }
+
+        iosMain {
+            dependencies {
+            }
+        }
+
+        jsMain {
+            dependencies {
+            }
+        }
+
+        jvmMain {
+            dependencies {
+            }
+        }
+    }
+
 }
