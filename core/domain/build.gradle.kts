b86d10a2bfeed6b21762e5a194a323e44b78be45
puria kazemieh <puriakazemieh@gmail.com>
2026-04-15 17:03:16 +0330

    [REFACTOR] Change core:domain from Android library to kmp library

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,10 +1,74 @@
 plugins {
-    id("convention.kotlin.library")
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.android.lint)
     id("convention.kotlin.serialization")
     id("convention.koin")
 }
 
-dependencies {
-    implementation(project(":core:common"))
-    implementation(libs.kotlinx.coroutines.core)
+kotlin {
+    androidLibrary {
+        namespace = "com.kazemieh.domain"
+        compileSdk {
+            version = release(36)
+        }
+        minSdk = 24
+    }
+
+    val xcfName = "core:commonKit"
+
+    iosX64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    iosSimulatorArm64 {
+        binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    js {
+        browser()
+    }
+
+    jvm()
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(project(":core:common"))
+            }
+        }
+
+
+        androidMain {
+            dependencies {
+            }
+        }
+
+        iosMain {
+            dependencies {
+            }
+        }
+
+        jsMain {
+            dependencies {
+            }
+        }
+
+        jvmMain {
+            dependencies {
+            }
+        }
+    }
+
 }
