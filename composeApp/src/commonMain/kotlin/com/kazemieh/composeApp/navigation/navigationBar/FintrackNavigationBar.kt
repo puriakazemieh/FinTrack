b62215ed8a569e58636036c15dd9471a490aaf14
puriakazemieh <puriakazemieh@gmail.com>
2026-05-11 13:07:37 +0330

    [REFACTOR] Run in web and jvm

--- composeApp/src/commonMain/kotlin/com/kazemieh/composeApp/navigation/navigationBar/FintrackNavigationBar.kt
+++ composeApp/src/commonMain/kotlin/com/kazemieh/composeApp/navigation/navigationBar/FintrackNavigationBar.kt
@@ -15,8 +15,7 @@ import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.kazemieh.designsystem.component.FintrackLabelSmallText
-//import com.kazemieh.composeApp.navigation.Destinations
-//import fintrack.composeapp.generated.resources.ic_navigation_report
+import com.kazemieh.composeApp.navigation.Destinations
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
@@ -25,53 +24,53 @@ fun FintrackNavigationBar(
     modifier: Modifier = Modifier,
     navController: NavHostController
 ) {
-//    val currentDestination = navController.currentBackStackEntryAsState().value?.destination
-//    val selectedDestination by remember {
-//        derivedStateOf {
-//            val route = currentDestination?.route.toString()
-//            when {
-//                route.contains(Destinations.DASHBOARD.route.toString()) -> Destinations.DASHBOARD
-//                route.contains(Destinations.REPORT.route.toString()) -> Destinations.REPORT
-//                route.contains(Destinations.SETTING.route.toString()) -> Destinations.SETTING
-//                else -> Destinations.DASHBOARD
-//            }
-//        }
-//    }
-//    NavigationBar(modifier = modifier) {
-//        Destinations.entries.forEach { destination ->
-//            val selected = selectedDestination.route == destination.route
-//            NavigationBarItem(
-//                selected = selected,
-//                label = {
-//                    FintrackLabelSmallText(stringResource(destination.label))
-//                },
-//                icon = {
-////                    Icon(
-//////                        painter = painterResource(destination.icon),
-////                        painter = painterResource(fintrack.composeapp.generated.resources.Res.drawable.ic_navigation_report),
-//////                        imageVector = ImageVector.vectorResource(id = destination.icon),
-////                        contentDescription = stringResource(destination.label)
-////                    )
-//                },
-//                onClick = {
-//                    if (!selected) {
-//                        navController.navigate(destination.route) {
-//                            popUpTo(navController.graph.findStartDestination().id) {
-//                                saveState = true
-//                            }
-//                            launchSingleTop = true
-//                            restoreState = true
-//                        }
-//                    }
-//                },
-//                colors = NavigationBarItemDefaults.colors(
-//                    selectedIconColor = MaterialTheme.colorScheme.primary,
-//                    selectedTextColor = MaterialTheme.colorScheme.primary,
-//                    unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
-//                    unselectedTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
-//                    indicatorColor = Color.Transparent
-//                ),
-//            )
-//        }
-//    }
+    val currentDestination = navController.currentBackStackEntryAsState().value?.destination
+    val selectedDestination by remember {
+        derivedStateOf {
+            val route = currentDestination?.route.toString()
+            when {
+                route.contains(Destinations.DASHBOARD.route.toString()) -> Destinations.DASHBOARD
+                route.contains(Destinations.REPORT.route.toString()) -> Destinations.REPORT
+                route.contains(Destinations.SETTING.route.toString()) -> Destinations.SETTING
+                else -> Destinations.DASHBOARD
+            }
+        }
+    }
+    NavigationBar(modifier = modifier) {
+        Destinations.entries.forEach { destination ->
+            val selected = selectedDestination.route == destination.route
+            NavigationBarItem(
+                selected = selected,
+                label = {
+                    FintrackLabelSmallText(stringResource(destination.label))
+                },
+                icon = {
+                    Icon(
+                        painter = painterResource(destination.icon),
+//                        painter = painterResource(fintrack.composeapp.generated.resources.Res.drawable.ic_navigation_report),
+//                        imageVector = ImageVector.vectorResource(id = destination.icon),
+                        contentDescription = stringResource(destination.label)
+                    )
+                },
+                onClick = {
+                    if (!selected) {
+                        navController.navigate(destination.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                    selectedTextColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                    unselectedTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                    indicatorColor = Color.Transparent
+                ),
+            )
+        }
+    }
 }
